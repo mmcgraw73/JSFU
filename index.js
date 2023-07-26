afcc9ad57a8f4383076acb05e1ddee5f1c47a352
mmcgraw73 <shout@mcgraw.io>
2023-07-26 13:11:06 -0400

    constructor function

--- index.js
+++ index.js
@@ -4,4 +4,24 @@ import './style.css';
 // Write Javascript code!
 const APP = document.getElementById('app');
 
-const Person = function ()
+
+/**
+ * constructor function
+ * 
+ * whats the difference b/w a constructor fn & and a regular fn?
+ * a constructor function is a regular function
+ * that requires the use of the reserved word 
+ * 'new' when called
+ * 
+ */
+
+const Dad = function (handle, kidCount) {
+  this.handle = handle
+  this.kidCount = kidCount
+}
+
+let michael = new Dad('Supafly', 2)
+
+
+APP.innerHTML = `which dad? ${michael.handle} 
+how many kids? ${michael.kidCount}`
