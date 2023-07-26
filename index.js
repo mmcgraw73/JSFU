ae29fa8228846e978a48ff189d18d27eaaf6a4b8
mmcgraw73 <shout@mcgraw.io>
2023-07-26 13:15:24 -0400

    new keyword explained

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const APP = document.getElementById('app');
  * that requires the use of the reserved word 
  * 'new' when called
  * 
+ * what happens when 'new' is called?
+ * 1) a {} (aka: emptyO) is created
+ * 2) the function is called using emptyO as the context of 'this'
+ * 3) emptyO is linked to prototype
+ * 4) the function automatically returns emptyO
+ * 
  */
 
 const Dad = function (handle, kidCount) {
